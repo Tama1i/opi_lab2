2a3e32be835ac2b44e53189dec332d6f8b1b3649
Tama1i <ryazantseff.matvei@yandex.ru>
2023-03-24 10:01:47 +0300

    32

--- individual/lb2idz/main.py
+++ individual/lb2idz/main.py
@@ -6,22 +6,22 @@ def gl(f):
         return 1
 
 if __name__ == "__main__":
-    file = open("f1.txt", encoding='UTF')
-    f = file.read()
-    f = f.lower()
-    f = " " + f + "\n"
-    s = ""
-    i = 1
-    ot = ""
-    for i in range(len(f)-1):
-        if (f[i-1] == " ") or (f[i-1] == "\n"):
-            k = 1
-            if (gl(f[i]) == 1):
-                k = 2
-        if k == 2:
-            s = s + f[i]
-            if (f[i] == " ") or (f[i] == "\n"):
-                k = 0
-                ot = s + " "
+    with open("f1.txt","r", encoding="utf-8") as file:
+        f = file.read()
+        f = f.lower()
+        f = " " + f + "\n"
+        s = ""
+        i = 1
+        ot = ""
+        for i in range(len(f)-1):
+            if (f[i-1] == " ") or (f[i-1] == "\n"):
+                k = 1
+                if (gl(f[i]) == 1):
+                    k = 2
+            if k == 2:
+                s = s + f[i]
+                if (f[i] == " ") or (f[i] == "\n"):
+                    k = 0
+                    ot = s + " "
 
-    print(ot)
+        print(ot)
