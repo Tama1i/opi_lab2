76ddd6461f1f14428da62ab84f536736448ea58b
Tama1i <ryazantseff.matvei@yandex.ru>
2023-03-27 11:47:44 +0300

    requirements

--- individual/lb2idz2/main.py
+++ individual/lb2idz2/main.py
@@ -4,15 +4,13 @@
 if __name__ == "__main__":
     re = str(input("ishodnic - "))
     wr = str(input("cuda - "))
-    w = 1
     q = ""
     ot = ""
-    with open(re,"r", encoding="utf-8") as file:
+    with open(re, "r", encoding="utf-8") as file:
         ff = file.readlines()
-    for i in ff:
-        q = (f"{str(w)}; {i}")
-        w += 1
-        ot = (f'{ot}{q}\n')
+    for i, ii in enumerate(ff):
+        q = '; '.join([str(i + 1), ii])
+        ot = ''.join([ot, q])
     with open(wr, "a", encoding="utf-8") as qe:
         qe.write(ot)
 
--- os/main.py
+++ os/main.py
@@ -4,21 +4,20 @@
 import os
 
 if __name__ == "__main__":
-    re = str(input("ishodnic - "))
-    wr = str(input("cuda - "))
-    ka = str(input("catalog - "))
+    re = input("ishodnic - ")
+    wr = input("cuda - ")
+    ka = input("catalog - ")
     p = os.getcwd()
     path = os.path.join(p, ka)
     os.makedirs(path)
-    w = 1
     q = ""
     ot = ""
-    with open(re,"r", encoding="utf-8") as file:
+    with open(re, "r", encoding="utf-8") as file:
         ff = file.readlines()
-    for i in ff:
-        q = (f"{str(w)}; {i}")
-        w += 1
-        ot = (f'{ot}{q}\n')
+    for i, ii in enumerate(ff):
+        q = '; '.join([str(i+1), ii])
+        ot = ''.join([ot, q])
+
     os.chdir(path)
     with open(wr, "a", encoding="utf-8") as ge:
         ge.write(ot)
